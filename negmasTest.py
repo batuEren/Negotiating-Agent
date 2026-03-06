70c4a1c0b339b94564c5b543a8b83389307e9ef0
Batu Eren <batu.eren2004@gmail.com>
2026-03-06 15:32:03 +0100

    init

---
+++ negmasTest.py
@@ -0,0 +1,37 @@
+#from https://negmas.readthedocs.io/en/latest/tutorials/01.running_simple_negotiation.html#a-simple-bilateral-negotiation
+
+from negmas import (
+    make_issue,
+    SAOMechanism,
+    NaiveTitForTatNegotiator,
+    TimeBasedConcedingNegotiator,
+)
+from negmas.preferences import LinearAdditiveUtilityFunction as LUFun
+from negmas.preferences.value_fun import LinearFun, IdentityFun, AffineFun
+
+# create negotiation agenda (issues)
+issues = [
+    make_issue(name="price", values=10),
+    make_issue(name="quantity", values=(1, 11)),
+    make_issue(name="delivery_time", values=10),
+]
+# create the mechanism
+session = SAOMechanism(issues=issues, n_steps=20)
+# define buyer and seller utilities
+seller_utility = LUFun(
+    values=[IdentityFun(), LinearFun(0.2), AffineFun(-1, bias=9.0)],
+    outcome_space=session.outcome_space,
+)
+buyer_utility = LUFun(
+    values={
+        "price": AffineFun(-1, bias=9.0),
+        "quantity": LinearFun(0.2),
+        "delivery_time": IdentityFun(),
+    },
+    outcome_space=session.outcome_space,
+)
+# create and add buyer and seller negotiators
+session.add(TimeBasedConcedingNegotiator(name="buyer"), preferences=buyer_utility)
+session.add(TimeBasedConcedingNegotiator(name="seller"), ufun=seller_utility)
+# run the negotiation and show the results
+print(session.run())
