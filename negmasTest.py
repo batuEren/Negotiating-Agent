8a78f462fe71e9967ece6a61762cf6ba478e2249
Yunus Mert Aliskan <Y.M.Alskan@student.tudelft.nl>
2026-03-24 19:08:41 +0100

    Tit tat Agent (#8)
    * Create titTat.py
    
    * tittat
    
    * tit tat not working
    
    * Update titTat.py
    
    * Fix to tit tat
    
    ---------
    
    Co-authored-by: Kaan Altıntaş <145195436+KaanAres10@users.noreply.github.com>

--- negmasTest.py
+++ negmasTest.py
@@ -1,14 +1,15 @@
 #from https://negmas.readthedocs.io/en/latest/tutorials/01.running_simple_negotiation.html#a-simple-bilateral-negotiation
 
+import matplotlib.pyplot as plt
 from negmas import (
     make_issue,
     SAOMechanism,
-    TimeBasedConcedingNegotiator,
 )
 from negmas.preferences import LinearAdditiveUtilityFunction as LUFun
 from negmas.preferences.value_fun import LinearFun, IdentityFun, AffineFun
 from microNegotiator import MicroNegotiator
 from timeBasedAgent import TimeBasedAgent
+from titTat import TitForTatAgent
 
 # create negotiation agenda (issues)
 issues = [
@@ -32,20 +33,6 @@ buyer_utility = LUFun(
     outcome_space=session.outcome_space,
 )
 # create and add buyer and seller negotiators
-
-session.add(MicroNegotiator(name="buyer"), preferences=buyer_utility)
-# session.add(TimeBasedConcedingNegotiator(name="seller"), ufun=seller_utility)
-
-# session.add(
-#     TimeBasedAgent(
-#         name="buyer",
-#         reservation_ratio=0.4,
-#         concession_curve="reverse_log",
-#         reverse_log_k=12.0,
-#     ),
-#     ufun=buyer_utility,
-# )
-
 session.add(
     TimeBasedAgent(
         name="seller",
@@ -55,7 +42,7 @@ session.add(
     ),
     ufun=seller_utility,
 )
-
 # run the negotiation and show the results
 print(session.run())
 session.plot(show_reserved=False)
+plt.show()
